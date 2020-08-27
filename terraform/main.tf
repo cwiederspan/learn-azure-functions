e25bee18e79d212f8a6a5b886347a983eeecfef4
Chris Wiederspan <chris@wiederspan.com>
2020-08-27 02:05:57 +0000

    Updated to include networking and private links in Terraform files

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,9 +21,50 @@ variable "location" {
   description = "The Azure region where the resources will be created."
 }
 
+variable "cosmos_ip_range_filter" {
+  type        = string
+  description = "A comma separated list of CIDR block strings that can access the Cosmos DB data."
+  # default     = null
+}
+
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_name
   location = var.location
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.resource_name}-vnet"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "web" {
+  name                 = "web-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+
+  service_endpoints = ["Microsoft.AzureCosmosDB"]
+
+  delegation {
+    name = "web-delegation"
+
+    service_delegation {
+      name    = "Microsoft.Web/serverFarms"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
+    }
+  }
+}
+
+resource "azurerm_subnet" "data" {
+  name                 = "data-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.10.0/24"]
+
+  enforce_private_link_endpoint_network_policies = true
+  # enforce_private_link_service_network_policies  = true
 }
 
 resource "azurerm_storage_account" "storage" {
@@ -34,12 +75,56 @@ resource "azurerm_storage_account" "storage" {
   account_replication_type = "LRS"
 }
 
+resource "azurerm_cosmosdb_account" "cosmos" {
+  name                = "${var.resource_name}-cosmos"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  offer_type          = "Standard"
+  kind                = "GlobalDocumentDB"
+
+  enable_automatic_failover         = false
+  is_virtual_network_filter_enabled = true
+
+  consistency_policy {
+    consistency_level       = "BoundedStaleness"
+    max_interval_in_seconds = 10
+    max_staleness_prefix    = 200
+  }
+
+  geo_location {
+    location          = azurerm_resource_group.rg.location
+    failover_priority = 0
+  }
+
+  virtual_network_rule {
+    id = azurerm_subnet.web.id
+  }
+
+  ip_range_filter = var.cosmos_ip_range_filter
+}
+
+resource "azurerm_cosmosdb_sql_database" "database" {
+  name                = "MyDatabase"
+  account_name        = azurerm_cosmosdb_account.cosmos.name
+  resource_group_name = azurerm_cosmosdb_account.cosmos.resource_group_name
+  throughput          = 400
+}
+
+resource "azurerm_cosmosdb_sql_container" "data" {
+  name                = "MyData"
+  account_name        = azurerm_cosmosdb_account.cosmos.name
+  resource_group_name = azurerm_cosmosdb_account.cosmos.resource_group_name
+  database_name       = azurerm_cosmosdb_sql_database.database.name
+  partition_key_path  = "/id"
+  throughput          = 400
+}
+
 resource "azurerm_app_service_plan" "plan" {
   name                = "${var.resource_name}-plan"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   
-  kind             = "linux"
+  kind             = "elastic"  # "linux"
   is_xenon         = false
   per_site_scaling = false
   reserved         = true
@@ -51,8 +136,6 @@ resource "azurerm_app_service_plan" "plan" {
     size     = "EP1"
     capacity = 1
   }
-
-  # tags = null
 }
 
 resource "azurerm_function_app" "app" {
@@ -63,4 +146,33 @@ resource "azurerm_function_app" "app" {
   storage_account_name       = azurerm_storage_account.storage.name
   storage_account_access_key = azurerm_storage_account.storage.primary_access_key
   os_type                    = "linux"
+}
+
+resource "azurerm_private_endpoint" "endpoint" {
+  name                = "${var.resource_name}-privatelink"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  subnet_id           = azurerm_subnet.data.id
+
+  private_service_connection {
+    name                           = "${var.resource_name}-privatelink-connection"
+    private_connection_resource_id = azurerm_cosmosdb_account.cosmos.id
+    subresource_names              = ["Sql"]
+    is_manual_connection           = false
+  }
+
+  private_dns_zone_group {
+    name                 = "${var.resource_name}-privatelink-dns"
+    private_dns_zone_ids = [azurerm_private_dns_zone.dns.id]
+  }
+}
+
+resource "azurerm_private_dns_zone" "dns" {
+  name                = "privatelink.documents.azure.com"
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_app_service_virtual_network_swift_connection" "injection" {
+  app_service_id = azurerm_function_app.app.id
+  subnet_id      = azurerm_subnet.web.id
 }
